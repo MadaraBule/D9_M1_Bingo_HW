c2a1580a930238b047d9808b85ccfadf7f34d09e
MadaraBule <100915562+MadaraBule@users.noreply.github.com>
2022-03-10 16:10:37 +0200

    progress on bingo

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 const fields = 76;
+let bingo = [
+    1,
+    2
+]
 function displayNumberFields() {
     let numberNode = document.getElementById("bingo");
     for (let number = 1; number <= fields; number++) {
@@ -8,6 +12,14 @@ function displayNumberFields() {
         numberSquare.addEventListener("click", selectNumber)
         numberNode.appendChild(numberSquare)
     }
+}
+function selectNumber(event) {
+    let clickedNumberNode = event.target
+    let previouslySelectedNumber = document.querySelector('.selected-number')
+    if (previouslySelectedNumber !== null) {
+        previouslySelectedNumber.classList.remove('selected-number')
+    }
+    clickedDayNode.classList.add('selected-number')
 }
 
 function onLoad() {
